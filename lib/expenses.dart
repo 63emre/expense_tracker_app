c34f3731ffe273e41a2c8ea4ddee03ffe968ebd8
Emre Atilla <emre.atilla@4hera.com>
2025-10-02 15:47:18 +0300

    mock data _registeredExpenses displayed on the screen

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,6 @@
+import 'package:expense_tracker_app/models/expense.dart';
 import 'package:flutter/material.dart';
+import 'expenses_list.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -10,10 +12,35 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+    Expense(
+      title: 'Groceries',
+      amount: 150.12,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [Text('Chart'), Text('List of expenses')]),
+      body: Column(
+        children: [
+          const Text('Chart'),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+        ],
+      ),
     );
   }
 }
--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,19 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
-  
+
+enum Category { food, leisure, travel, work }
+
 class Expense {
-  Expense({required this.title, required this.amount, required this.date})
-    : id = uuid.v4();
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
   final String title;
   final double amount;
   final String id;
   final DateTime date;
-
-
-  final String category;
-    food,
-    leisure,
-    travel,
-    work,
-}}
+  final Category category;
+}
