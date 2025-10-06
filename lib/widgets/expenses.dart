9fcc9b64e10f04d4a6764e2925fae012cfcda773
Emre Atilla <emreanlatilla@gmail.com>
2025-10-06 09:34:44 +0300

    removing expenses from expenses list using Dismissible method

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -47,6 +47,12 @@ class _ExpensesState extends State<Expenses> {
     });
   }
 
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -63,7 +69,12 @@ class _ExpensesState extends State<Expenses> {
       body: Column(
         children: [
           const Text('Chart'),
-          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+          Expanded(
+            child: ExpensesList(
+              expenses: _registeredExpenses,
+              onRemoveExpense: _removeExpense,
+            ),
+          ),
         ],
       ),
     );
